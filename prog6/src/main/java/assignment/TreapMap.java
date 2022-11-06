7246bc018bcbff5b60aec51dc69c183750a90d5f
Aditya Chebrolu <adityarchebrolu@gmail.com>
2022-11-06 16:08:16 -0600

    fix bugs and testing

--- prog6/src/main/java/assignment/TreapMap.java
+++ prog6/src/main/java/assignment/TreapMap.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
+public class TreapMap<K extends Comparable<K>, V> implements Treap<K, V> {
 
     private class Node {
         private Node leftChild;
@@ -39,7 +39,6 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
             if (leftChild != null) {
                 leftChild.setParent(this);
             }
-
         }
 
         private void setRightChild(Node rightChild) {
@@ -78,24 +77,20 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
         }
 
         @Override
-        public boolean equals(Object o) {
-            Node curr = (Node) o;
-            return this.key.equals(curr.getKey());
-        }
-
-        @Override
         public String toString() {
-            return "[" + this.getPriority() + "] <" + this.getKey() + ", " + this.getValue();
+            return "[" + this.getPriority() + "] <" + this.getKey() + ", " + this.getValue() + ">";
         }
     }
 
+    // Root node of tree
     private Node root;
 
-    public MyTreap() {
+    public TreapMap() {
 
     }
 
-    private MyTreap(Node n) {
+    // Private constructor for internal usage
+    private TreapMap(Node n) {
         this.root = n;
     }
 
@@ -108,23 +103,30 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
         return lookupHelper(root, key);
     }
 
+    // Recursive helper method for lookup function
     private V lookupHelper(Node currNode, K key) {
-        if (key.compareTo(currNode.getKey()) < 0) {
-            lookupHelper(currNode.getLeftChild(), key);
-        } else if (key.compareTo(currNode.getKey()) > 0) {
-            lookupHelper(currNode.getLeftChild(), key);
-        } else {
-            return currNode.getValue();
+        if (currNode == null) {
+            return null;
         }
 
-        return null;
+        V result;
+
+        if (key.compareTo(currNode.getKey()) < 0) {
+            result = lookupHelper(currNode.getLeftChild(), key);
+        } else if (key.compareTo(currNode.getKey()) > 0) {
+            result = lookupHelper(currNode.getRightChild(), key);
+        } else {
+            result = currNode.getValue();
+        }
+
+        return result;
     }
 
     @Override
     public void insert(K key, V value) {
         Node insertedNode = new Node(key, value);
 
-        if (this.root == null) {
+        if (this.root == null) { // Check if there are no nodes in the tree
             this.root = insertedNode;
         } else {
             placeNodeBST(insertedNode, this.root);
@@ -133,7 +135,6 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
     }
 
     // Helper method to place node in Treap according to BST property
-    // Return parent of Node space to be inserted
     private void placeNodeBST(Node insertedNode, Node currNode) {
         if (insertedNode.getKey().compareTo(currNode.getKey()) < 0) {
             if (currNode.getLeftChild() == null) {
@@ -142,6 +143,7 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
                 placeNodeBST(insertedNode, currNode.getLeftChild());
             }
 
+            // Determine whether to percolate upwards
             if (currNode.getLeftChild().getPriority() > currNode.getPriority()) {
                 rotateRight(currNode);
             }
@@ -152,14 +154,16 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
                 placeNodeBST(insertedNode, currNode.getRightChild());
             }
 
+            // Determine whether to percolate upwards
             if (currNode.getRightChild().getPriority() > currNode.getPriority()) {
                 rotateLeft(currNode);
             }
-        } else {
+        } else { // If node is already present, update value
             currNode.setValue(insertedNode.getValue());
         }
     }
 
+    // Helper method used after rotation to fix rotated node to parent node
     private void updateParent(Node parent, Node pivot, Node pivotChild) {
         if (parent == null) {
             this.root = pivotChild;
@@ -171,8 +175,7 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
         }
     }
 
-    // Given parent root where x is the left child of y:
-    // Rotate right around y
+    // Perform a right rotation around pivot node
     private void rotateRight(Node pivot) {
         Node parent = pivot.getParent();
         Node pivotLeft = pivot.getLeftChild();
@@ -181,8 +184,7 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
         updateParent(parent, pivot, pivotLeft);
     }
 
-    // Given parent root where x is the right child of y:
-    // Rotate left around y
+    // Perform a left rotation around the pivot node
     private void rotateLeft(Node pivot) {
         Node parent = pivot.getParent();
         Node pivotRight = pivot.getRightChild();
@@ -190,7 +192,6 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
         pivotRight.setLeftChild(pivot);
         updateParent(parent, pivot, pivotRight);
     }
-
 
     @Override
     public V remove(K key) {
@@ -203,11 +204,16 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
             return null;
         }
 
+        V currValue;
+
         if (deleteKey.compareTo(currNode.getKey()) < 0) {
-            removeHelper(currNode.getLeftChild(), deleteKey);
+            currValue = removeHelper(currNode.getLeftChild(), deleteKey);
         } else if (deleteKey.compareTo(currNode.getKey()) > 0) {
-            removeHelper(currNode.getRightChild(), deleteKey);
+            currValue = removeHelper(currNode.getRightChild(), deleteKey);
         } else {
+            // We have found the node to be removed
+            currValue = currNode.getValue();
+
             if (currNode.isLeafNode()) {
                 removeLeaf(currNode);
             } else if (currNode.getLeftChild() != null && currNode.getRightChild() != null) {
@@ -218,20 +224,21 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
                 }
                 removeHelper(currNode, deleteKey);
             } else if (currNode.getLeftChild() != null) {
+                // If only left child is present, we rotate right
                 rotateRight(currNode);
                 removeHelper(currNode, deleteKey);
             } else {
+                // If only right child is present, we rotate left
                 rotateLeft(currNode);
                 removeHelper(currNode, deleteKey);
             }
-
-            return currNode.getValue();
         }
 
-        return null;
+        // Node is not found in tree
+        return currValue;
     }
 
-    // Helper method to remove leaf node of Treap
+    // Helper method to remove a leaf node in Treap
     private void removeLeaf(Node currNode) {
         Node parent = currNode.getParent();
 
@@ -246,22 +253,25 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
 
 
     @Override
-    public Treap[] split(K key) {
-        Treap[] splits = new MyTreap[2];
+    public Treap<K, V>[] split(K key) {
+        Treap<K, V>[] splits = new TreapMap[2];
 
         if (root == null) {
             return splits;
         }
 
+        // Add new dummy node with Infinite priority
         Node n = new Node(key, null, Integer.MAX_VALUE);
         this.placeNodePriority(n, root);
 
-        splits[0] = new MyTreap(root.getLeftChild());
-        splits[1] = new MyTreap(root.getRightChild());
+        splits[0] = new TreapMap(root.getLeftChild());
+        splits[1] = new TreapMap(root.getRightChild());
 
         return splits;
     }
 
+    // Helper method for split method to insert node as root
+    // Additionally accounts for split calls w/ duplicate key
     private void placeNodePriority(Node insertedNode, Node currNode) {
         if (insertedNode.getKey().compareTo(currNode.getKey()) <= 0) {
             if (currNode.getLeftChild() == null) {
@@ -288,15 +298,18 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
 
     @Override
     public void join(Treap t) {
-        MyTreap myT = (MyTreap) t;
+        TreapMap myT = (TreapMap) t;
         Node tRoot = myT.root;
+
         if (root != null) {
+            // Set dummy node as root and then remove dummy node
             Node dummy = new Node(root.key, root.value);
             dummy.setLeftChild(this.root);
             dummy.setRightChild(tRoot);
             this.root = dummy;
             remove(root.key);
         } else {
+            // If tree is empty, we simply set the parameter to this tree
             this.root = tRoot;
         }
     }
@@ -312,20 +325,20 @@ public class MyTreap<K extends Comparable<K>, V> implements Treap<K, V> {
     }
 
     @Override
-    public Iterator iterator() {
-        List<Node> nodes = new ArrayList<>();
+    public Iterator<K> iterator() {
+        List<K> nodes = new ArrayList<>();
         iteratorHelper(nodes, root);
         return nodes.iterator();
     }
 
-    // Pre Order Traversal
-    private void iteratorHelper(List<Node> nodes, Node currNode) {
+    // In order traversal for iterator
+    private void iteratorHelper(List<K> nodes, Node currNode) {
         if (currNode == null) {
             return;
         }
 
         iteratorHelper(nodes, currNode.getLeftChild());
-        nodes.add(currNode);
+        nodes.add(currNode.getKey());
         iteratorHelper(nodes, currNode.getRightChild());
     }
 
--- prog6/src/test/java/assignment/Tests.java
+++ prog6/src/test/java/assignment/Tests.java
@@ -3,8 +3,128 @@ package assignment;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-public class Tests extends MyTreap {
+import java.util.Iterator;
 
+public class Tests {
 
+    @Test
+    public void checkLookupContains() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
 
+        Assertions.assertTrue(t.lookup(1) != null);
+    }
+
+    @Test
+    public void checkLookupNoContain() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
+
+        Assertions.assertTrue(t.lookup(5) == null);
+    }
+
+    @Test
+    public void checkInsertOne() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
+
+        Iterator iter = t.iterator();
+        boolean test = false;
+
+        while (iter.hasNext()) {
+            int curr = (int) iter.next();
+            if (curr == 2) {
+                test = true;
+            }
+        }
+
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void checkInsertValue() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
+
+        boolean test = t.lookup(1) == 0 && t.lookup(2) == 5 && t.lookup(6) == 9;
+
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void checkInsertDuplicate() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
+        t.insert(1, 2);
+
+        Assertions.assertTrue(t.lookup(1) == 2);
+    }
+
+    @Test
+    public void checkRemovePresent() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(2, 5);
+        t.insert(6, 9);
+        t.insert(7, 8);
+        t.insert(5, 2);
+        t.insert(3, 3);
+
+        Assertions.assertTrue(t.remove(1) == 0);
+    }
+
+    @Test
+    public void checkIteratorSize() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(3, 3);
+        t.insert(2, 5);
+        t.insert(7, 8);
+        t.insert(6, 9);
+        t.insert(5, 2);
+
+        Iterator iter = t.iterator();
+
+        int count = 0;
+
+        while (iter.hasNext()) {
+            iter.next();
+            count++;
+        }
+
+        Assertions.assertTrue(count == 6);
+    }
+
+    @Test
+    public void checkIteratorSizeDuplicate() {
+        Treap<Integer, Integer> t = new TreapMap<>();
+        t.insert(1, 0);
+        t.insert(3, 3);
+        t.insert(2, 5);
+        t.insert(7, 8);
+        t.insert(6, 9);
+        t.insert(5, 2);
+        t.insert(5, 21);
+
+        Iterator iter = t.iterator();
+
+        int count = 0;
+
+        while (iter.hasNext()) {
+            iter.next();
+            count++;
+        }
+
+        Assertions.assertTrue(count == 6);
+    }
 }
